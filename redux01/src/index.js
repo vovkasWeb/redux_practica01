e35605eb1e30d2c27362199e76b869004ad15204
vovkasWeb <vova77557@gmail.com>
2023-11-15 14:59:01 +0200

    teoria

--- redux01/src/index.js
+++ redux01/src/index.js
@@ -1,58 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-
-const initialState = { value: 0 }
-
-const reducer = (state = initialState, action) => {
-	switch (action.type) {
-		case 'INC':
-			return {
-				...state,
-				value: state.value + 1,
-			}
-		case 'DEC':
-			return {
-				...state,
-				value: state.value - 1,
-			}
-		case 'RND':
-			return {
-				...state,
-				value: state.value * action.payload,
-			}
-		default:
-			return state
-	}
-}
-
-// let state = reducer(initialState,{type: 'INC'})
-// state = reducer(state, { type: 'INC' })
-// console.log(state);
+import { createStore, bindActionCreators } from 'redux'
+import reducer from './reducer'
+import * as actions from './actions'
 
 const store = createStore(reducer)
 
+const { dispatch, subscribe, getState } = store
+
 const update = () => {
-	document.getElementById('counter').textContent = store.getState().value
+	document.getElementById('counter').textContent = getState().value
 }
 
-store.subscribe(update)
+subscribe(update)
 
-const inc = () => ({ type: 'INC' })
-const dec = () => ({ type: 'DEC' })
-const rnd = value => ({ type: 'RND', payload: value })
+// const bindActionCreator = (creator,dispatch) => (...args)=>{
+// 	dispatch(creator(...args))
+// }
 
-document.getElementById('inc').addEventListener('click', () => {
-	store.dispatch(inc())
-})
+const { inc, dec, rnd } = bindActionCreators(actions,dispatch)
 
-document.getElementById('dec').addEventListener('click', () => {
-	store.dispatch(dec())
-})
+document.getElementById('inc').addEventListener('click', inc)
+
+document.getElementById('dec').addEventListener('click', dec)
 
 document.getElementById('rnd').addEventListener('click', () => {
 	const value = Math.floor(Math.random() * 10)
-	store.dispatch(rnd(value))
+	rnd(value)
 })
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
