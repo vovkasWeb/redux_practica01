f84df3d905937f23c516b8f9d8010b26f54b8388
vovkasWeb <vova77557@gmail.com>
2023-11-17 21:13:52 +0200

    add: fooks

--- redux01/src/components/Counter.js
+++ redux01/src/components/Counter.js
@@ -1,31 +1,34 @@
-import { connect } from 'react-redux'
-import * as actions from '../actions'
+
+import { inc, dec, rnd } from '../actions'
 import { bindActionCreators } from 'redux'
-const Counter = ({ counter, inc, dec, rnd }) => {
+import { useSelector, useDispatch } from 'react-redux'
+const Counter = () => {
+	const counter = useSelector(state => state.counter)
+	const dispatch = useDispatch();
 	return (
 		<div className='jumbotron'>
 			<h1>{counter}</h1>
-			<button onClick={dec} className='btn btn-primary'>
+			<button onClick={() => dispatch(dec())} className='btn btn-primary'>
 				DEC
 			</button>
-			<button onClick={inc} className='btn btn-primary'>
+			<button onClick={() => dispatch(inc())} className='btn btn-primary'>
 				INC
 			</button>
-			<button onClick={rnd} className='btn btn-primary'>
+			<button onClick={() => dispatch(rnd())} className='btn btn-primary'>
 				RND
 			</button>
 		</div>
 	)
 }
 
-const mapStateToPtops = state => {
-	return {
-		counter: state.value,
-	}
-}
+// const mapStateToPtops = state => {
+// 	return {
+// 		counter: state.value,
+// 	}
+// }
 // const mapDispatchToProps = dispatch => {
 // 	return  bindActionCreators(actions, dispatch)
 
 // }
-
-export default connect(mapStateToPtops, actions)(Counter)
+// export default connect(mapStateToPtops, actions)(Counter)
+export default Counter
